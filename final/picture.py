cb4ad3cf3d8627fcdcffc1695cfba7c1eeead1c7
mudbri <mubashiranwar@live.com>
2022-05-13 11:25:03 +0100

    Final

---
+++ final/picture.py
@@ -0,0 +1,29 @@
+from picamera import PiCamera
+import time
+import pytesseract
+from PIL import Image
+import cv2
+
+camera = PiCamera()
+camera.resolution = (1280, 720)
+#camera.vflip = True
+#camera.contrast = 10
+time.sleep(2)
+for i in range(10):
+    camera.capture("img3.jpg")
+    time.sleep(2)
+    img = cv2.imread('img3.jpg',cv2.IMREAD_COLOR) #Open the image from which charectors has to be recognized
+    img = cv2.resize(img, (620,480) ) #resize the image if required
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to grey to reduce detials
+    gray = cv2.bilateralFilter(gray, 11, 17, 17) #Blur to reduce noise
+
+    original = pytesseract.image_to_string(gray, config='')
+    test = (pytesseract.image_to_data(gray, lang=None, config='', nice=0) ) #get confidence level if required
+    print(pytesseract.image_to_boxes(gray))
+
+    print (original)
+    #text = pytesseract.image_to_string(img, config='')
+    #print (text)
+
+print("Done.")
